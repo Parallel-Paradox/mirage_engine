3852974be23b87386ca5306b33ab9d8f5de6b84e
wangzekai <wangzekai.parallel@bytedance.com>
2024-02-04 11:21:16 +0800

    Detect apple and windows os.

--- src/mirage_framework/define.hpp
+++ src/mirage_framework/define.hpp
@@ -5,7 +5,15 @@
 #include <cassert>
 #endif
 
-#if defined(MIRAGE_BUILD_SHARED) && defined(_WIN32)
+// OS detector
+#if defined(_WIN32)
+#define OS_WIN
+#elif defined(__APPLE__)
+#define OS_APPLE
+#endif
+
+// dllexport on MSVC
+#if defined(MIRAGE_BUILD_SHARED) && defined(OS_WIN)
 #if defined(MIRAGE_BUILD)
 #define MIRAGE_API __declspec(dllexport)
 #else
@@ -15,6 +23,7 @@
 #define MIRAGE_API
 #endif
 
+// Debug checker
 #if defined(MIRAGE_BUILD_DEBUG)
 #define MIRAGE_DCHECK(condition) assert(condition)
 #else
