2a75ec6dbf11ec9bece6139139bca3939bcd4742
wangzekai <wangzekai.parallel@bytedance.com>
2024-03-06 16:32:40 +0800

    Better iteration test of singly linked list.

--- test/mirage_framework/linked_list_tests.cpp
+++ test/mirage_framework/linked_list_tests.cpp
@@ -4,14 +4,31 @@
 
 using namespace mirage;
 
-TEST(SinglyLinkedListTests, Iteration) {
+TEST(SinglyLinkedListTests, Iterate) {
+  using Iter = SinglyLinkedList<int32_t>::Iterator;
+  using ConstIter = SinglyLinkedList<int32_t>::ConstIterator;
+
   EXPECT_TRUE(std::forward_iterator<SinglyLinkedList<int32_t>::Iterator>);
   EXPECT_TRUE(std::forward_iterator<SinglyLinkedList<int32_t>::ConstIterator>);
 
   SinglyLinkedList<int32_t> list = {0, 1, 2};
+
   int cnt = 0;
-  for (auto num : list) {
+  for (int32_t num : list) {
     EXPECT_EQ(cnt, num);
+    ++cnt;
+  }
+
+  cnt = 0;
+  for (Iter iter = list.begin(); iter != list.end(); ++iter) {
+    EXPECT_EQ(cnt, *iter);
+    ++cnt;
+  }
+
+  cnt = 0;
+  const SinglyLinkedList<int32_t>& const_list = list;
+  for (ConstIter iter = const_list.begin(); iter != const_list.end(); ++iter) {
+    EXPECT_EQ(cnt, *iter);
     ++cnt;
   }
 }
