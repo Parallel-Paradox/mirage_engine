f1c74bd58525a3de76d21ff2cc8eb0f7413ece0e
wangzekai <wangzekai.parallel@outlook.com>
2025-02-05 14:16:19 +0800

    Implement singly linked list.

---
+++ libs/mirage_base/container/singly_linked_list.hpp
@@ -0,0 +1,439 @@
+#ifndef MIRAGE_BASE_CONTAINER_SINGLY_LINKED_LIST
+#define MIRAGE_BASE_CONTAINER_SINGLY_LINKED_LIST
+
+#include <initializer_list>
+
+#include "mirage_base/define.hpp"
+
+namespace mirage::base {
+
+template <std::move_constructible T>
+class SinglyLinkedList {
+ public:
+  struct Node;
+
+  class Iterator;
+  class ConstIterator;
+
+  SinglyLinkedList() = default;
+
+  SinglyLinkedList(SinglyLinkedList&& other) noexcept;
+  SinglyLinkedList& operator=(SinglyLinkedList&& other) noexcept;
+
+  SinglyLinkedList(const SinglyLinkedList& other)
+    requires std::copy_constructible<T>;
+  SinglyLinkedList& operator=(const SinglyLinkedList& other)
+    requires std::copy_constructible<T>;
+
+  SinglyLinkedList(std::initializer_list<T> list)
+    requires std::copy_constructible<T>;
+
+  ~SinglyLinkedList();
+
+  template <typename... Args>
+  void EmplaceHead(Args&&... args);
+  void PushHead(const T& val)
+    requires std::copy_constructible<T>;
+
+  T RemoveHead();
+  void Clear();
+
+  Iterator begin();
+  Iterator end();
+
+  ConstIterator begin() const;
+  ConstIterator end() const;
+
+ private:
+  Node* head_{nullptr};
+};
+
+template <std::move_constructible T>
+struct SinglyLinkedList<T>::Node {
+  T val;
+  Node* next{nullptr};
+
+  Node() = delete;
+  Node(Node&&) = delete;
+  Node(const Node&) = delete;
+  ~Node() = default;
+
+  explicit Node(T&& val) : val(std::move(val)) {}
+};
+
+template <std::move_constructible T>
+class SinglyLinkedList<T>::Iterator {
+ public:
+  using iterator_concept = std::forward_iterator_tag;
+  using iterator_category = std::forward_iterator_tag;
+  using iterator_type = Iterator;
+  using difference_type = int64_t;
+  using value_type = T;
+  using pointer = value_type*;
+  using reference = value_type&;
+
+  Iterator() = default;
+  ~Iterator() = default;
+
+  Iterator(const Iterator& other);
+
+  explicit Iterator(Node* here);
+
+  iterator_type& operator=(const iterator_type& other);
+  iterator_type& operator=(std::nullptr_t);
+  reference operator*() const;
+  pointer operator->() const;
+  iterator_type& operator++();
+  iterator_type operator++(int);
+  bool operator==(const iterator_type& other) const;
+  bool operator==(std::nullptr_t) const;
+  explicit operator bool() const;
+
+  template <typename... Args>
+  void EmplaceAfter(Args&&... args);
+  void InsertAfter(const T& val)
+    requires std::copy_constructible<T>;
+  T RemoveAfter();
+
+ private:
+  friend class ConstIterator;
+
+  Node* here_{nullptr};
+};
+
+template <std::move_constructible T>
+class SinglyLinkedList<T>::ConstIterator {
+ public:
+  using iterator_concept = std::forward_iterator_tag;
+  using iterator_category = std::forward_iterator_tag;
+  using iterator_type = ConstIterator;
+  using difference_type = int64_t;
+  using value_type = const T;
+  using pointer = value_type*;
+  using reference = value_type&;
+
+  ConstIterator() = default;
+  ~ConstIterator() = default;
+
+  ConstIterator(const ConstIterator& other);
+
+  explicit ConstIterator(Node* here);
+
+  // NOLINTNEXTLINE: Convert to const
+  ConstIterator(const Iterator& iter);
+
+  iterator_type& operator=(const iterator_type& other);
+  iterator_type& operator=(std::nullptr_t);
+  reference operator*() const;
+  pointer operator->() const;
+  iterator_type& operator++();
+  iterator_type operator++(int);
+  bool operator==(const iterator_type& other) const;
+  bool operator==(std::nullptr_t) const;
+  explicit operator bool() const;
+
+ private:
+  Node* here_{nullptr};
+};
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::SinglyLinkedList(SinglyLinkedList&& other) noexcept
+    : head_(other.head_) {
+  other.head_ = nullptr;
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>& SinglyLinkedList<T>::operator=(
+    SinglyLinkedList&& other) noexcept {
+  if (this != &other) {
+    Clear();
+    new (this) SinglyLinkedList(std::move(other));
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::SinglyLinkedList(const SinglyLinkedList& other)
+  requires std::copy_constructible<T>
+{
+  auto iter = other.begin();
+  if (iter == other.end()) {
+    return;
+  }
+  Node* ptr = new Node(T(*iter));
+  head_ = ptr;
+  ++iter;
+  while (iter != other.end()) {
+    Node* next = new Node(T(*iter));
+    ptr->next = next;
+    ptr = next;
+    ++iter;
+  }
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>& SinglyLinkedList<T>::operator=(
+    const SinglyLinkedList& other)
+  requires std::copy_constructible<T>
+{
+  if (this != &other) {
+    Clear();
+    new (this) SinglyLinkedList(other);
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::SinglyLinkedList(std::initializer_list<T> list)
+  requires std::copy_constructible<T>
+{
+  if (list.size() == 0) {
+    return;
+  }
+  auto iter = list.begin();
+  Node* ptr = new Node(T(*iter));
+  head_ = ptr;
+  ++iter;
+  while (iter != list.end()) {
+    Node* next = new Node(T(*iter));
+    ptr->next = next;
+    ptr = next;
+    ++iter;
+  }
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::~SinglyLinkedList() {
+  Clear();
+}
+
+template <std::move_constructible T>
+template <typename... Args>
+void SinglyLinkedList<T>::EmplaceHead(Args&&... args) {
+  Node* new_head = new Node(T(std::forward<Args>(args)...));
+  new_head->next = head_;
+  head_ = new_head;
+}
+
+template <std::move_constructible T>
+void SinglyLinkedList<T>::PushHead(const T& val)
+  requires std::copy_constructible<T>
+{
+  EmplaceHead(T(val));
+}
+
+template <std::move_constructible T>
+T SinglyLinkedList<T>::RemoveHead() {
+  MIRAGE_DCHECK(head_ != nullptr);
+  T val(std::move(head_->val));
+  const Node* head = head_;
+  head_ = head_->next;
+  delete head;
+  return std::move(val);
+}
+
+template <std::move_constructible T>
+void SinglyLinkedList<T>::Clear() {
+  Node* ptr = head_;
+  while (ptr != nullptr) {
+    Node* next = ptr->next;
+    delete ptr;
+    ptr = next;
+  }
+  head_ = nullptr;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator SinglyLinkedList<T>::begin() {
+  return Iterator(head_);
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator SinglyLinkedList<T>::end() {
+  return Iterator();
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator SinglyLinkedList<T>::begin() const {
+  return ConstIterator(head_);
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator SinglyLinkedList<T>::end() const {
+  return ConstIterator();
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::Iterator::Iterator(const Iterator& other)
+    : here_(other.here_) {}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::Iterator::Iterator(Node* here) : here_(here) {}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator::iterator_type&
+SinglyLinkedList<T>::Iterator::operator=(const iterator_type& other) {
+  if (this != &other) {
+    here_ = other.here_;
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator::iterator_type&
+SinglyLinkedList<T>::Iterator::operator=(std::nullptr_t) {
+  here_ = nullptr;
+  return *this;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator::reference
+SinglyLinkedList<T>::Iterator::operator*() const {
+  return here_->val;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator::pointer
+SinglyLinkedList<T>::Iterator::operator->() const {
+  return &(here_->val);
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator::iterator_type&
+SinglyLinkedList<T>::Iterator::operator++() {
+  if (here_ != nullptr) {
+    here_ = here_->next;
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::Iterator::iterator_type
+SinglyLinkedList<T>::Iterator::operator++(int) {
+  if (here_ != nullptr) {
+    iterator_type temp = *this;
+    here_ = here_->next;
+    return temp;
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+bool SinglyLinkedList<T>::Iterator::operator==(
+    const iterator_type& other) const {
+  return here_ == other.here_;
+}
+
+template <std::move_constructible T>
+bool SinglyLinkedList<T>::Iterator::operator==(std::nullptr_t) const {
+  return here_ == nullptr;
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::Iterator::operator bool() const {
+  return here_ != nullptr;
+}
+
+template <std::move_constructible T>
+template <typename... Args>
+void SinglyLinkedList<T>::Iterator::EmplaceAfter(Args&&... args) {
+  Node* new_node = new Node(T(std::forward<Args>(args)...));
+  new_node->next = here_->next;
+  here_->next = new_node;
+}
+
+template <std::move_constructible T>
+void SinglyLinkedList<T>::Iterator::InsertAfter(const T& val)
+  requires std::copy_constructible<T>
+{
+  EmplaceAfter(T(val));
+}
+
+template <std::move_constructible T>
+T SinglyLinkedList<T>::Iterator::RemoveAfter() {
+  MIRAGE_DCHECK(here_ != nullptr && here_->next != nullptr);
+  T val(std::move(here_->next->val));
+  const Node* next = here_->next;
+  here_->next = nullptr;
+  delete next;
+  return std::move(val);
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::ConstIterator::ConstIterator(const ConstIterator& other)
+    : here_(other.here_) {}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::ConstIterator::ConstIterator(Node* here) : here_(here) {}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::ConstIterator::ConstIterator(const Iterator& iter)
+    : here_(iter.here_) {}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator::iterator_type&
+SinglyLinkedList<T>::ConstIterator::operator=(const iterator_type& other) {
+  if (this != &other) {
+    here_ = other.here_;
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator::iterator_type&
+SinglyLinkedList<T>::ConstIterator::operator=(std::nullptr_t) {
+  here_ = nullptr;
+  return *this;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator::reference
+SinglyLinkedList<T>::ConstIterator::operator*() const {
+  return here_->val;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator::pointer
+SinglyLinkedList<T>::ConstIterator::operator->() const {
+  return &here_->val;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator::iterator_type&
+SinglyLinkedList<T>::ConstIterator::operator++() {
+  if (here_ != nullptr) {
+    here_ = here_->next;
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+typename SinglyLinkedList<T>::ConstIterator::iterator_type
+SinglyLinkedList<T>::ConstIterator::operator++(int) {
+  if (here_ != nullptr) {
+    iterator_type temp = *this;
+    here_ = here_->next;
+    return temp;
+  }
+  return *this;
+}
+
+template <std::move_constructible T>
+bool SinglyLinkedList<T>::ConstIterator::operator==(
+    const iterator_type& other) const {
+  return here_ == other.here_;
+}
+
+template <std::move_constructible T>
+bool SinglyLinkedList<T>::ConstIterator::operator==(std::nullptr_t) const {
+  return here_ == nullptr;
+}
+
+template <std::move_constructible T>
+SinglyLinkedList<T>::ConstIterator::operator bool() const {
+  return here_ != nullptr;
+}
+
+}  // namespace mirage::base
+
+#endif  // MIRAGE_BASE_CONTAINER_SINGLY_LINKED_LIST
---
+++ tests/mirage_base/container/singly_linked_list.cpp
@@ -0,0 +1,116 @@
+#include <gtest/gtest.h>
+
+#include "mirage_base/auto_ptr/owned.hpp"
+#include "mirage_base/container/singly_linked_list.hpp"
+
+using namespace mirage::base;
+
+namespace {
+
+struct Counter final {
+  int32_t* base_destructed{nullptr};
+
+  explicit Counter(int32_t* base_destructed)
+      : base_destructed(base_destructed) {}
+
+  ~Counter() { *base_destructed += 1; }
+};
+
+}  // namespace
+
+TEST(SinglyLinkedListTests, Iterate) {
+  using Iter = SinglyLinkedList<int32_t>::Iterator;
+  using ConstIter = SinglyLinkedList<int32_t>::ConstIterator;
+
+  EXPECT_TRUE(std::forward_iterator<SinglyLinkedList<int32_t>::Iterator>);
+  EXPECT_TRUE(std::forward_iterator<SinglyLinkedList<int32_t>::ConstIterator>);
+
+  SinglyLinkedList<int32_t> list = {0, 1, 2};
+
+  int cnt = 0;
+  for (int32_t num : list) {
+    EXPECT_EQ(cnt, num);
+    ++cnt;
+  }
+  EXPECT_EQ(cnt, 3);
+
+  cnt = 0;
+  for (Iter iter = list.begin(); iter != list.end(); ++iter) {  // NOLINT
+    EXPECT_EQ(cnt, *iter);
+    ++cnt;
+  }
+  EXPECT_EQ(cnt, 3);
+
+  cnt = 0;
+  const SinglyLinkedList<int32_t>& const_list = list;
+  // NOLINTNEXTLINE
+  for (ConstIter iter = const_list.begin(); iter != const_list.end(); ++iter) {
+    EXPECT_EQ(cnt, *iter);
+    ++cnt;
+  }
+  EXPECT_EQ(cnt, 3);
+}
+
+TEST(SinglyLinkedListTests, SetIterator) {
+  using Iter = SinglyLinkedList<int32_t>::Iterator;
+  using ConstIter = SinglyLinkedList<int32_t>::ConstIterator;
+
+  SinglyLinkedList<int32_t> list = {0, 1, 2};
+
+  Iter iter_a = list.begin();
+  Iter iter_b;  // NOLINT: Test setter
+  iter_b = iter_a;
+  EXPECT_EQ(iter_a, iter_b);
+  EXPECT_TRUE(iter_a);
+  iter_a = nullptr;
+  EXPECT_EQ(iter_a, nullptr);
+  EXPECT_EQ(iter_b, list.begin());
+
+  ConstIter const_iter_a = list.begin();
+  ConstIter const_iter_b;  // NOLINT: Test setter
+  const_iter_b = const_iter_a;
+  EXPECT_EQ(const_iter_a, const_iter_b);
+  EXPECT_TRUE(const_iter_a);
+  const_iter_a = nullptr;
+  EXPECT_EQ(const_iter_a, nullptr);
+  EXPECT_EQ(const_iter_b, list.begin());
+}
+
+TEST(SinglyLinkedListTests, Destruct) {
+  int32_t destruct_cnt = 0;
+  {
+    SinglyLinkedList<Owned<Counter>> list;
+    list.EmplaceHead(Owned<Counter>::New(&destruct_cnt));
+    list.begin().EmplaceAfter(Owned<Counter>::New(&destruct_cnt));
+  }
+  EXPECT_EQ(destruct_cnt, 2);
+}
+
+TEST(SinglyLinkedListTests, Remove) {
+  SinglyLinkedList<int32_t> list = {0, 1};
+  EXPECT_EQ(list.begin().RemoveAfter(), 1);
+  EXPECT_EQ(list.RemoveHead(), 0);
+  EXPECT_EQ(list.begin(), list.end());
+}
+
+TEST(SinglyLinkedListTests, MoveAndCopy) {
+  SinglyLinkedList<int32_t> list = {0, 1};
+  SinglyLinkedList<int32_t> move_list(std::move(list));
+  SinglyLinkedList<int32_t> copy_list(move_list);
+
+  EXPECT_EQ(list.begin(), list.end());
+
+  int cnt = 0;
+  for (int32_t num : move_list) {
+    EXPECT_EQ(cnt, num);
+    ++cnt;
+  }
+  EXPECT_EQ(cnt, 2);
+
+  cnt = 0;
+  for (int32_t num : copy_list) {
+    EXPECT_EQ(cnt, num);
+    ++cnt;
+  }
+  EXPECT_EQ(cnt, 2);
+}
