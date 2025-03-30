e8cb8d4110b36019f5a0407552fd1c79cbfe6354
wangzekai <wangzekai.parallel@outlook.com>
2025-03-30 19:58:17 +0800

    Extractor of system.

---
+++ libs/mirage_ecs/system/context.hpp
@@ -0,0 +1,12 @@
+#ifndef MIRAGE_ECS_SYSTEM_CONTEXT
+#define MIRAGE_ECS_SYSTEM_CONTEXT
+
+#include "mirage_ecs/define.hpp"
+
+namespace mirage::ecs {
+
+class MIRAGE_ECS Context{};
+
+}  // namespace mirage::ecs
+
+#endif  // MIRAGE_ECS_SYSTEM_CONTEXT
---
+++ libs/mirage_ecs/system/param_extract.hpp
@@ -0,0 +1,11 @@
+#ifndef MIRAGE_ECS_SYSTEM_PARAM_EXTRACT
+#define MIRAGE_ECS_SYSTEM_PARAM_EXTRACT
+
+namespace mirage::ecs {
+
+template <typename T>
+struct Extractor {};
+
+}  // namespace mirage::ecs
+
+#endif  // MIRAGE_ECS_SYSTEM_PARAM_EXTRACT
