1d0483abb9d7e6d1f737b6aad47291dc7d07659b
wangzekai <wangzekai.parallel@outlook.com>
2025-03-31 21:13:32 +0800

    Check query params.

--- libs/mirage_ecs/system/query.hpp
+++ libs/mirage_ecs/system/query.hpp
@@ -1,43 +1,63 @@
 #ifndef MIRAGE_ECS_SYSTEM_QUERY
 #define MIRAGE_ECS_SYSTEM_QUERY
 
+#include <tuple>
+
+#include "mirage_ecs/marker.hpp"
 #include "mirage_ecs/system/extract.hpp"
 
 namespace mirage::ecs {
 
-template <typename... Args>
-struct Ref {
-  constexpr static bool IsValid() {
-    return true;  // TODO
-  }
-};
+struct QueryParamsTag {};
+
+template <typename T>
+concept IsQueryParams = std::derived_from<T, QueryParamsTag>;
 
 template <typename... Args>
-struct With {
-  constexpr static bool IsValid() {
-    return true;  // TODO
-  }
-};
+concept IsQueryParamsList = ((IsQueryParams<Args>) && ...);
+
+// --- Ref ---
+
+struct QueryParamsTag_Ref : QueryParamsTag {};
 
 template <typename... Args>
-struct Without {
-  constexpr static bool IsValid() {
-    return true;  // TODO
-  }
+  requires IsComponentRefList<Args...>
+struct Ref : QueryParamsTag_Ref {
+  using TypeList = std::tuple<Args...>;
 };
+
+// --- With ---
+
+struct QueryParamsTag_With : QueryParamsTag {};
+
+template <typename... Args>
+  requires IsComponentList<Args...>
+struct With : QueryParamsTag_With {
+  using TypeList = std::tuple<Args...>;
+};
+
+// --- Without ---
+
+struct QueryParamsTag_Without : QueryParamsTag {};
+
+template <typename... Args>
+  requires IsComponentList<Args...>
+struct Without : QueryParamsTag_Without {
+  using TypeList = std::tuple<Args...>;
+};
+
+// ----------
 
 template <typename... Arg>
+  requires IsQueryParamsList<Arg...>
 class Query {
  public:
-  constexpr static bool IsValid() {
-    return true;  // TODO
-  }
+ private:
 };
 
 template <typename... Args>
 struct Extract<Query<Args...>> {
   static Query<Args...> From([[maybe_unused]] Context& context) {
-    static_assert(Query<Args...>::IsValid());
     return Query<Args...>();
   }
 };
--- tests/mirage_ecs/system/query_tests.cpp
+++ tests/mirage_ecs/system/query_tests.cpp
@@ -4,8 +4,43 @@
 
 using namespace mirage::ecs;
 
-TEST(QueryTests, Extract) {
-  using ValidQuery =
-      Query<Ref<int32_t&, const size_t&>, With<int32_t>, Without<bool>>;
+struct Position : Component {
+  float x;
+  float y;
+};
+
+struct Velocity : Component {
+  float x;
+  float y;
+};
+
+struct WithTag : Component {};
+
+struct WithoutTag : Component {};
+
+TEST(QueryTests, ParamsConceptCheck) {
+  bool is_component_ref_list = IsComponentRefList<Position&, const Velocity&>;
+  EXPECT_TRUE(is_component_ref_list);
+  is_component_ref_list = IsComponentRefList<int32_t&>;
+  EXPECT_FALSE(is_component_ref_list);
+  is_component_ref_list = IsComponentRefList<Position>;
+  EXPECT_FALSE(is_component_ref_list);
+
+  bool is_component_list = IsComponentList<Position, Velocity>;
+  EXPECT_TRUE(is_component_list);
+  is_component_list = IsComponentList<int32_t>;
+  EXPECT_FALSE(is_component_list);
+
+  bool is_query_params_list =
+      IsQueryParamsList<Ref<Position&, const Velocity&>, With<WithTag>,
+                        Without<WithoutTag>>;
+  EXPECT_TRUE(is_query_params_list);
+  is_query_params_list = IsQueryParamsList<int32_t>;
+  EXPECT_FALSE(is_query_params_list);
+}
+
+TEST(QueryTests, ExtractQuery) {
+  using ValidQuery = Query<Ref<Position&, const Velocity&>, With<WithTag>,
+                           Without<WithoutTag>>;
   EXPECT_TRUE(ExtractType<ValidQuery>);
 }
