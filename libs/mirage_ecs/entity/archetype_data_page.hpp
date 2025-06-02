1f21e830da444e6b613f07d117c2803ed75fd131
wangzekai.parallel <wangzekai.parallel@outlook.com>
2025-06-03 01:16:17 +0800

    Add operation interface.

--- libs/mirage_ecs/entity/archetype_data_page.hpp
+++ libs/mirage_ecs/entity/archetype_data_page.hpp
@@ -2,6 +2,7 @@
 #define MIRAGE_ECS_ENTITY_ARCHETYPE_DATA_PAGE
 
 #include "mirage_base/auto_ptr/shared.hpp"
+#include "mirage_ecs/component/component_bundle.hpp"
 #include "mirage_ecs/define/export.hpp"
 #include "mirage_ecs/entity/archetype_descriptor.hpp"
 
@@ -11,6 +12,9 @@ class ArchetypeDataPage {
   using SharedLocalDescriptor = base::SharedLocal<ArchetypeDescriptor>;
 
  public:
+  class View;
+  class Slice;
+
   ArchetypeDataPage() = delete;
   MIRAGE_ECS ~ArchetypeDataPage();
 
@@ -25,7 +29,22 @@ class ArchetypeDataPage {
   MIRAGE_ECS void Initialize(SharedLocalDescriptor descriptor);
   MIRAGE_ECS void Reset();
 
+  [[nodiscard]] MIRAGE_ECS bool Push(ComponentBundle& bundle);
+  [[nodiscard]] MIRAGE_ECS bool Push(Slice&& slice);
+  MIRAGE_ECS Slice SwapRemove(size_t index);
+
   MIRAGE_ECS void Clear();
+
+  MIRAGE_ECS View operator[](size_t index);
+  MIRAGE_ECS const View operator[](size_t index) const;
+
+  [[nodiscard]] MIRAGE_ECS const SharedLocalDescriptor& descriptor() const;
+  [[nodiscard]] MIRAGE_ECS size_t capacity() const;
+  [[nodiscard]] MIRAGE_ECS size_t size() const;
+
+  [[nodiscard]] MIRAGE_ECS std::byte* buffer_ptr() const;
+  [[nodiscard]] MIRAGE_ECS size_t align_padding() const;
+  [[nodiscard]] MIRAGE_ECS size_t byte_size() const;
 
  private:
   SharedLocalDescriptor descriptor_{nullptr};
